cdf3df3dc8eebd880c80574d98426b8a0a1ea12b
Pratham <pxathamm03@gmail.com>
2026-04-01 23:36:36 +0530

    largest 3 digit no.

---
+++ largest3digit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    // compare numbers
+    if (a >= b && a >= c)
+    {
+        cout << "Largest: " << a;
+    }
+    else if (b >= a && b >= c)
+    {
+        cout << "Largest: " << b;
+    }
+    else
+    {
+        cout << "Largest: " << c;
+    }
+
+    return 0;
+}
